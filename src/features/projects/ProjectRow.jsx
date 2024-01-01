4c426814ebbbabfa1130f1942b1b368dde7d1152
Mahla <107788139+MahlaParvaz@users.noreply.github.com>
2024-01-01 15:56:29 +0330

    feat:Modal

--- src/features/projects/ProjectRow.jsx
+++ src/features/projects/ProjectRow.jsx
@@ -1,35 +1,73 @@
-import toLocalDateShort from "../../utils/toLocalDateShort"
-import { toPersianNumbersWithComma } from "../../utils/toPersianNumbersWithComma"
-import truncateText from "../../utils/truncateText"
+import { HiOutlineTrash } from 'react-icons/hi';
+import Table from '../../ui/Table';
+import toLocalDateShort from '../../utils/toLocalDateShort';
+import { toPersianNumbersWithComma } from '../../utils/toPersianNumbersWithComma';
+import truncateText from '../../utils/truncateText';
+import Modal from '../../ui/Modal';
+import { TbPencilMinus } from 'react-icons/tb';
+import { useState } from 'react';
+import ConfirmDelete from '../../ui/ConfirmDelete';
 
-function ProjectRow({project, index}) {
+function ProjectRow({ project, index }) {
+  const [isEditOpen, setIsEditOpen] = useState(false);
+  const [isDeleteOpen, setIsDeleteOpen] = useState(false);
   return (
-    <Table.Row >
-    <td>{index + 1}</td>
-    <td>{truncateText(project.title, 30)}</td>
-    <td> {project.category.title}</td>
-    <td>{toPersianNumbersWithComma(project.budget)}</td>
-    <td>{toLocalDateShort(project.deadline)}</td>
-    <td>
-      <div className="flex flex-wrap items-center gap-2 max-w-[200px]">
-        {project.tags.map((tag) => (
-          <span className="badge badge--secondary" key={tag}>
-            {tag}
-          </span>
-        ))}
-      </div>
-    </td>
-    <td>{project.freelancer?.name || '-'}</td>
-    <td>
-      {project.status === 'OPEN' ? (
-        <span className="badge badge--success">باز</span>
-      ) : (
-        <span className="badge badge--danger">بسته</span>
-      )}
-    </td>
-    <td>...</td>
-  </Table.Row>
-  )
+    <Table.Row>
+      <td>{index + 1}</td>
+      <td>{truncateText(project.title, 30)}</td>
+      <td> {project.category.title}</td>
+      <td>{toPersianNumbersWithComma(project.budget)}</td>
+      <td>{toLocalDateShort(project.deadline)}</td>
+      <td>
+        <div className="flex flex-wrap items-center gap-2 max-w-[200px]">
+          {project.tags.map((tag) => (
+            <span className="badge badge--secondary" key={tag}>
+              {tag}
+            </span>
+          ))}
+        </div>
+      </td>
+      <td>{project.freelancer?.name || '-'}</td>
+      <td>
+        {project.status === 'OPEN' ? (
+          <span className="badge badge--success">باز</span>
+        ) : (
+          <span className="badge badge--danger">بسته</span>
+        )}
+      </td>
+      <td>
+        <div className="flex items-center gap-x-4">
+          <>
+            <button onClick={() => setIsEditOpen(true)}>
+              <TbPencilMinus className="w-5 h-5 text-primary-900" />
+            </button>
+            <Modal
+              title={`ویرایش ${project.title}`}
+              open={isEditOpen}
+              onClose={() => setIsEditOpen(false)}
+            ></Modal>
+          </>
+          <>
+            <button onClick={() => setIsDeleteOpen(true)}>
+              <HiOutlineTrash className="w-5 h-5 text-error" />
+            </button>
+            <Modal
+              title={`حذف ${project.title}`}
+              open={isDeleteOpen}
+              onClose={() => setIsDeleteOpen(false)}
+            >
+              <ConfirmDelete
+                resourceName={project.title}
+                onClose={() => setIsDeleteOpen(false)}
+                onConfirm={() => {}}
+                disabled={false}
+              />
+            </Modal>
+          </>
+        </div>
+      </td>
+    </Table.Row>
+  );
 }
 
-export default ProjectRow
+export default ProjectRow;
