be25585f40d2b8d0074e00f5b58dc929707aaeaf
Mahla <107788139+MahlaParvaz@users.noreply.github.com>
2024-01-02 13:02:46 +0330

    feat:Apply react hook form

--- src/features/authentication/AuthContainer.jsx
+++ src/features/authentication/AuthContainer.jsx
@@ -4,9 +4,12 @@ import CheckOTPForm from './CheckOTPForm';
 import toast from 'react-hot-toast';
 import { useMutation } from '@tanstack/react-query';
 import { getOtp } from '../../services/authService';
+import { useForm } from 'react-hook-form';
 function AuthContainer() {
   const [step, setStep] = useState(1);
-  const [phoneNumber, setPhoneNumber] = useState();
+  // const [phoneNumber, setPhoneNumber] = useState();
+  const { handleSubmit, register, getValues } = useForm();
+
   const {
     isPending: isSendingOtp,
     mutateAsync,
@@ -14,12 +17,11 @@ function AuthContainer() {
   } = useMutation({
     mutationFn: getOtp,
   });
-  const sendOtpHandler = async (e) => {
-    e.preventDefault();
+  const sendOtpHandler = async (data) => {
     try {
-      const data = await mutateAsync({ phoneNumber });
+      const { message } = await mutateAsync(data);
       setStep(2);
-      toast.success(data.message);
+      toast.success(message);
     } catch (error) {
       toast.error(error?.response?.data?.message);
     }
@@ -29,17 +31,19 @@ function AuthContainer() {
       case 1:
         return (
           <SendOTPForm
-            onSubmit={sendOtpHandler}
+            onSubmit={handleSubmit(sendOtpHandler)}
             isSendingOtp={isSendingOtp}
             setStep={setStep}
-            phoneNumber={phoneNumber}
-            onChange={(e) => setPhoneNumber(e.target.value)}
+            register={register}
+              // onSubmit={sendOtpHandler}
+            // phoneNumber={phoneNumber}
+            // onChange={(e) => setPhoneNumber(e.target.value)}
           />
         );
       case 2:
         return (
           <CheckOTPForm
-            phoneNumber={phoneNumber}
+            phoneNumber={getValues("phoneNumber")}
             onBack={() => setStep((s) => s - 1)}
             onReSendOtp={sendOtpHandler}
             otpResponse={otpResponse}
---
+++ src/features/projects/CreateProjectForm.jsx
@@ -0,0 +1,63 @@
+import { useState } from 'react';
+import TextField from '../../ui/TextField';
+import { useForm } from 'react-hook-form';
+function CreateProjectForm() {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <form className="space-y-8" onSubmit={handleSubmit(onSubmit)}>
+      <TextField
+        label="عنوان"
+        name="title"
+        register={register}
+        required
+        validationSchema={{
+          required: 'عنوان ضروری است',
+          minLength: {
+            value: 10,
+            message: 'حداقل 10 کاراکتر را وارد کنید',
+          },
+        }}
+        errors={errors}
+      />
+      <TextField
+        label="توضیحات"
+        name="description"
+        register={register}
+        required
+        validationSchema={{
+          required: 'توضیحات ضروری است',
+          minLength: {
+            value: 15,
+            message: 'حداقل 15 کاراکتر را وارد کنید',
+          },
+        }}
+        errors={errors}
+      />
+      <TextField
+        label="بودجه"
+        name="budget"
+        type="number"
+        register={register}
+        required
+        validationSchema={{
+          required: 'بودجه ضروری است',
+        }}
+        errors={errors}
+      />
+      <button type="submit" className="btn btn--primary w-full">
+        تایید
+      </button>
+    </form>
+  );
+}
+
+export default CreateProjectForm;
